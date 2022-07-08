30858debf4f788b923d8e82e2e9863d2f62da012
commoncode <zhao@commoncode.net>
2022-07-08 08:25:29 +0800

    第三次提交

---
+++ src/main/java/com/itheima/reggie/service/ShoppingCartService.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.ShoppingCart;
+
+/**
+ * @author COMMON-PC-1
+ */
+public interface ShoppingCartService extends IService<ShoppingCart> {
+
+}
