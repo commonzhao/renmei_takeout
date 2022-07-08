0517cd898055edcac55e67094db6f006d56021a5
commoncode <zhao@commoncode.net>
2022-07-08 08:21:20 +0800

    第三次提交

---
+++ src/main/java/com/itheima/reggie/service/DishFlavorService.java
@@ -0,0 +1,10 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.DishFlavor;
+
+/**
+ * @author COMMON-PC-1
+ */
+public interface DishFlavorService extends IService<DishFlavor> {
+}
