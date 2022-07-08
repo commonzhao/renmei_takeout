742fa9fd3b6c441d8a2673d2fe1985a5a72c4ddd
commoncode <zhao@commoncode.net>
2022-07-08 08:22:19 +0800

    第三次提交

---
+++ src/main/java/com/itheima/reggie/service/DishService.java
@@ -0,0 +1,30 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Dish;
+
+/**
+ * @author COMMON-PC-1
+ */
+public interface DishService extends IService<Dish> {
+
+    /***
+     * 新增菜品，同时插入菜品对应的口味数据，需要操作两张表：dish、dish_flavor
+     * @param dishDto dishDto
+     */
+    public void saveWithFlavor(DishDto dishDto);
+
+    /***
+     * 根据id查询菜品信息和对应的口味信息
+     * @param id id
+     * @return
+     */
+    public DishDto getByIdWithFlavor(Long id);
+
+    /***
+     * 更新菜品信息，同时更新对应的口味信息
+     * @param dishDto dishDto
+     */
+    public void updateWithFlavor(DishDto dishDto);
+}
